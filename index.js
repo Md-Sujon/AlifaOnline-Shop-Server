c62c5405072f962f39d2518378bc0e0185ef6a26
Md-Sujon <mdsujon829562@gmail.com>
2021-04-02 22:17:48 +0600

    redme file update

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 
 
@@ -86,7 +84,9 @@ app.delete('/delete/:id', (req, res) => {
 });
 
 
-
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
